e294135e325e63baee73dc7c7f6731ae04dcabe5
Alex <aandreba@gmail.com>
2020-06-07 12:11:56 +0200

    Update readcache_setup.sh

--- readcache_setup.sh
+++ readcache_setup.sh
@@ -14,6 +14,14 @@ if [ -d "$1" ]; then
   else
     # Caching directory doesn't exist
     echo "Warning: '${2} not found"
+    while true; do
+      read -p "Create directory '${2}'? (y/n) " yn
+      case $yn in
+          [Yy]* ) mkdir $2; break;;
+          [Nn]* ) exit 1;;
+          * ) echo "Please select a correct answer";;
+      esac
+    done
 else
   # Directory to cache doesn't exist
   echo "Error: '${1}' not found"
